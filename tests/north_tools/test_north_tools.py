5a5b21ad7893cb1504d5eb1b0ab57a9822f693c8
Lauri Himanen <lauri.himanen@gmail.com>
2026-03-27 10:37:40 +0200

    Updated link, config, folder structure.

--- tests/north_tools/test_north_tools.py
+++ tests/north_tools/test_north_tools.py
@@ -1,10 +1,8 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails for the north tool
-    from nomad_north_pyiron.north_tools.pyiron import (
-        north_tool_entry_point,
-    )
+    from nomad_north_pyiron.north_tools import pyiron
 
     assert (
-        north_tool_entry_point.id_url_safe == 'nomad_north_pyiron'
-        or north_tool_entry_point.id == 'nomad-north-pyiron'
+        pyiron.id_url_safe == 'pyiron'
+        or pyiron.id == 'nomad_north_pyiron.north_tools:pyiron'
     ), 'NORTHtool entry point has incorrect id or id_url_safe'
